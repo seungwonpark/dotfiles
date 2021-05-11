6d712dfde662ce07ba778b2d31e0281d17f17963
Seung-won Park 박승원 <yyyyy@snu.ac.kr>
2021-05-11 23:09:16 +0900

    add git configuration

--- setup.sh
+++ setup.sh
@@ -4,8 +4,14 @@ create_symlinks() {
     gln -rsf vim/.vimrc ~/.vimrc
 }
 
+configure_git() {
+    gln -rsf git/.gitconfig ~
+    gln -rsf git/.gitexclude ~
+}
+
 all() {
     create_symlinks
+    configure_git
 }
 
 if [ -n "$1" ]; then
