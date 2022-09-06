6a837abda9ab5e6554f2bea9baf10043195e1f66
Seung-won Park <yyyyy@snu.ac.kr>
2022-09-06 10:25:16 +0900

    add 'workon' alias

--- aliases.sh
+++ aliases.sh
@@ -1,2 +1,5 @@
 alias tb="tensorboard --logdir . --bind_all --samples_per_plugin=images=100 --port"
 alias jn="jupyter notebook --ip 0.0.0.0 --no-browser --port"
+workon() {
+    source "$HOME/venvs/$1/bin/activate"
+}
